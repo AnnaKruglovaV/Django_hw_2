a7344fe7e2b1cd6a68ee5765a1d4656902345d39
Anna Kruglova <anya.kruglova.16@gmail.com>
2024-07-08 19:37:50 +0400

    домашка_3

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework_3',
+        'USER': 'postgres',
+        'PASSWORD': '1409',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -123,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = ' media/'
+MEDIA_ROOT = BASE_DIR / 'media'
